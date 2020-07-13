4acd9fe0f693eb3fc590997aea84070ae4461a0f
Alessandro Del Sole <alessandro.delsole@outlook.com>
2020-07-13 21:18:01 +0200

    Initial check-in of module IncrementalHelloWorld

---
+++ IncrementalHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IncrementalHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
